c6a9a70228ec36b9b4787fb88e2bb19a3e879a41
Mateusz <janikowski.mateusz96@gmail.com>
2024-01-15 20:15:34 +0100

    update

--- pushover.py
+++ pushover.py
@@ -1,45 +1,43 @@
 import http.client
 import urllib
+import time
+import logging
 
 
 class PushoverNotifier:
     def __init__(self, app_token, user_key):
         self.app_token = app_token
         self.user_key = user_key
-        self.conn = http.client.HTTPSConnection("api.pushover.net:443")
 
-    def send_notification(self, message, title=None, url=None, url_title=None, priority=None, sound=None):
-        data = {
-            "token": self.app_token,
-            "user": self.user_key,
-            "message": message
-        }
+    def send_notification(self, message, title=None, url=None, url_title=None, priority=None, sound=None, retry_count=3, retry_delay=5):
+        for attempt in range(retry_count):
+            try:
+                conn = http.client.HTTPSConnection("api.pushover.net:443")
+                data = {
+                    "token": self.app_token,
+                    "user": self.user_key,
+                    "message": message
+                }
 
-        if title:
-            data["title"] = title
-        if url:
-            data["url"] = url
-        if url_title:
-            data["url_title"] = url_title
-        if priority:
-            data["priority"] = priority
-        if sound:
-            data["sound"] = sound
+                if title:
+                    data["title"] = title
+                if url:
+                    data["url"] = url
+                if url_title:
+                    data["url_title"] = url_title
+                if priority:
+                    data["priority"] = priority
+                if sound:
+                    data["sound"] = sound
 
-        self.conn.request("POST", "/1/messages.json",
-                          urllib.parse.urlencode(data),
-                          {"Content-type": "application/x-www-form-urlencoded"})
-        response = self.conn.getresponse()
-        return response.read()
-
-    def format_message(data):
-        return "\n".join(f"{key} {value}" for key, value in data.items()) + "\n\n"
-
-
-
-if __name__ == "__main__":
-    app_token = ""
-    user_key = ""
-
-    notifier = PushoverNotifier(app_token, user_key)
-    response = notifier.send_notification("TEST123", title="Test")
+                conn.request("POST", "/1/messages.json",
+                             urllib.parse.urlencode(data),
+                             {"Content-type": "application/x-www-form-urlencoded"})
+                response = conn.getresponse()
+                return response.read()
+            except http.client.RemoteDisconnected:
+                logging.warning("Remote end disconnected. Retrying in {} seconds...".format(retry_delay))
+                time.sleep(retry_delay)
+            finally:
+                conn.close()
+        raise Exception("Failed to send notification after {} attempts".format(retry_count))
